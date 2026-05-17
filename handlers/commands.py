1ca340220ce530c1e4f20f5021ee475b922f7c9a
Evgeniy Grakovich <grakovich-e@yandex.by>
2026-05-17 11:35:48 +0300

    spare state remove

--- handlers/commands.py
+++ handlers/commands.py
@@ -10,7 +10,7 @@ from states import State
 
 @dp.message_handler(commands=["start"], state="*")
 async def send_welcome(message: types.Message, state: FSMContext):
-    await state.finish()
+    # await state.finish()
     sections = await db.get_visible_bot_sections()
     await message.answer(texts.hello, reply_markup=kb.get_bot_sections_kb(sections))
     await State.menu.set()
